40f79e25e9c1c29d2ff701506677898c87340a1c
Elodie Rié <elodierie@yahoo.fr>
2021-08-16 22:11:24 +0200

    ajout produit.js

--- produit.js
+++ produit.js
@@ -1,36 +1,40 @@
-// fetch('http://localhost:3000/api/teddies/' + id)
-//   .then(response => response.json())
-//   .then(data => {
-//     const articlesContainer = document.getElementById('product');
-//     for (let articles of data) {
-//       articlesContainer.innerHTML += 
-//       `<a href="${articles._id}" class="productCard">
-//       <img class="productImage" src="${articles.imageUrl}"> 
-//       <div class="productStory">
-//       <div class="namePrice">
-//         <h1 class="productName">
-//           ${articles.name}
-//         </h1> 
-//         <div class="productPrice">
-//           ${articles.price * 0.01}€
-//         </div>
-//        </div>
-//        <div class="productDescription">
-//       <br> ${articles.description}<br></div></div></a>`;
-//     }
-//   });
+ function getContent(id) {fetch('http://localhost:3000/api/teddies/' + id)
+   .then(response => response.json())
+   .then(data => {
+     const articlesContainer = document.getElementById('product');
+     
+// const articles = data; (remplacer articles par data dans la suite du code)
+       articlesContainer.innerHTML += 
+       `<a href="${articles._id}" class="productCard">
+       <img class="productImage" src="${articles.imageUrl}"> 
+       <div class="productStory">
+       <div class="namePrice">
+         <h1 class="productName">
+           ${articles.name}
+         </h1> 
+         <div class="productPrice">
+           ${articles.price * 0.01}€
+         </div>
+        </div>
+        <div class="productDescription">
+       <br> ${articles.description}<br></div></div></a>;
+       <button id="button" onClick=${clickHandler(data)}></button>`;
+  })
+};
 
-// //récupération de l'ID de l'ours de la page
-// const queryString = window.location.search;
-// const urlParams = new URLSearchParams(queryString);
-// const id = urlParams.get('id');
-// console.log(id);
+document.addEventListener("DOMcontentLoaded", () => {
+    const queryString = window.location.search;
+     const urlParams = new URLSearchParams(queryString);
+    const id = urlParams.get('id');
+     console.log(id);
+    getContent(id)
+});
 
-// const getBear = async function() {
-//     // récupération des données de l'ours sélectionné par son id
-//    try {
-//        let response = await fetch('http://localhost:3000/api/teddies/' + id);
-//        if (response.ok) {
-//            let bear = await response.json();
-//            console.log(bear);
+function clickHandler(data) {
+    //Ajouter data au local localStorage
+const array = localStorage.getItem('monPanier')
 
+array.push(data)
+
+localStorage.setItem("monPanier", JSON.stringify(init));
+};
