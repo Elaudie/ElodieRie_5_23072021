5dd7bab1ffdc2573048c4342f2ea1d58872f0c85
Elodie Rié <elodierie@yahoo.fr>
2021-08-24 11:04:34 +0200

    page produit

--- produit.js
+++ produit.js
@@ -4,24 +4,26 @@
      const articlesContainer = document.getElementById('product');
      
        articlesContainer.innerHTML += 
-       `<a href="${data._id}" class="productCard">
-       <img class="productImage" src="${data.imageUrl}"> 
-       <div class="productStory">
-       <div class="namePrice">
-         <h1 class="productName">
+       `<a href="${data._id}" class="cardProduct">
+       <img class="imageProduct" src="${data.imageUrl}"> 
+       <div class="storyProduct">
+       <div class="priceName">
+         <h1 class="NameProduct">
            ${data.name}
          </h1> 
-         <div class="productPrice">
+         <div class="priceProduct">
            ${data.price * 0.01}€
          </div>
         </div>
-        <div class="productDescription">
-       <br> ${data.description}<br></div></div></a>;
-       <button id="button" onClick=${clickHandler(data)}></button>`;
-  })
-};
+        <div class="descriptionProduct">
+       <br> ${data.description}<br></div></div>
+       <button id="button" onClick='clickHandler(${JSON.stringify(
+        data
+     )})'>Ajouter au panier</button></a>`  
+    })
+}
 
-document.addEventListener("DOMcontentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => {
     const queryString = window.location.search;
      const urlParams = new URLSearchParams(queryString);
     const id = urlParams.get('id');
@@ -30,10 +32,29 @@ document.addEventListener("DOMcontentLoaded", () => {
 });
 
 function clickHandler(data) {
-    //Ajouter data au local localStorage
-const array = localStorage.getItem('monPanier')
+  //Ajouter data au local localStorage
 
-array.push(data)
+  // On stocke window.localStorage dans une variable
+  // pour l'écrire plus rapidement par la suite
+  const localStorage = window.localStorage;
 
-localStorage.setItem("monPanier", JSON.stringify(init));
-};
+  // On stocke l'élément "monPanier" qui doit se trouver
+  // dans localStorage. On décode la chaîne de caractères
+  // avec JSON.parse
+  let panier = JSON.parse(localStorage.getItem("monPanier"));
+
+  // Si l'élément est nul on initialise un tableau (array) vide
+  if (!panier || !panier.length) {
+    panier = [];
+  }
+
+  // On pousse les données du produit dans le tableau (array)
+  panier.push(data);
+
+  // Dernière étape, on remplace l'élément monPanier du localStorage
+  // par la nouvelle version qu'on vient de créer.
+  // Pour stocker dans localStorage il faut transformer l'objet en chaîne de caractères
+  // d'où la fonction JSON.stringify
+  localStorage.setItem("monPanier", JSON.stringify(panier));
+}
+
