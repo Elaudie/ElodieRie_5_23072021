387d1c77397dcba2fcf53e2da4d4dcfca710745d
Elodie Rié <elodierie@yahoo.fr>
2021-08-30 11:34:43 +0200

    refonte html + js

--- confirmation.js
+++ confirmation.js
@@ -1,6 +1,14 @@
+/* Afficher l'orderId */
 let receivedOrderId = localStorage.getItem('orderId');
-
 displayOrderId();
 function displayOrderId() {
     document.getElementById('orderid').innerText = receivedOrderId;
+}
+
+/* Afficher le prix total */
+displayTotalPrice()
+function displayTotalPrice() {
+    let totalPriceInCart = localStorage.getItem('TotalPrice');
+    totalPriceInCart = JSON.parse(totalPriceInCart);
+    document.getElementById('totalprice').textContent = totalPriceInCart.toLocaleString("fr-FR", {style:"currency", currency:"EUR"});
 }
