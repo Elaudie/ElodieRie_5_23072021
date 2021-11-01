cdf560a153705ccfead577a4bb73ef9c09d00cdb
Elodie Rié <elodierie@yahoo.fr>
2021-11-01 21:16:33 +0100

    correction indentation

--- produit.js
+++ produit.js
@@ -34,7 +34,7 @@
 
 document.addEventListener("DOMContentLoaded", () => {
     const queryString = window.location.search;
-     const urlParams = new URLSearchParams(queryString);
+    const urlParams = new URLSearchParams(queryString);
     const id = urlParams.get('id');
      console.log(id);
     getContent(id)
